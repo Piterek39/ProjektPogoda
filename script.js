abbe0e5ed814e4785c112dc05dc005ab915d1f40
Piotr Dudek <piotr.dudek@microsoft.wsei.edu.pl>
2024-05-29 09:38:37 +0200

    end

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-//const input = document.querySelector('input')
-//const button = document.querySelector('button')
-//const cityName = document.querySelector('.city-name')
-//const warning = document.querySelector('.warning')
-//const photo = document.querySelector('.photo')
-//const weather = document.querySelector('.weather')
-//const temperature = document.querySelector('.temperature')
-//const humidity = document.querySelector('.humidity')
-
-// https://openweathermap.org/
-// https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Web_mechanics/What_is_a_URL
 const apiKey = 'd24a8078f06d1d158e682e4cc6b8f24b';
-//const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric';
-//const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric';
 
 document.querySelector('button').addEventListener('click', () => {
     const city = document.querySelector('input').value;
